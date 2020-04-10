f103e04af61c237003117b74e459b9b2aec0dbdf
Ashutosh Wadhvekar <awadhvekar@hawk.iit.edu>
2020-04-10 01:06:43 -0500

    Added ngx-bootstrap: Header elements changed, FA icon link added, dropdown added

--- README.txt
+++ README.txt
@@ -2,3 +2,4 @@ Angular Frontend code:
 
 npm i ngx-progressbar
 npm install ngx-alerts --save
+npm install ngx-bootstrap --save
--- sportshunt_frontend/src/app/shared/shared.module.ts
+++ sportshunt_frontend/src/app/shared/shared.module.ts
@@ -8,6 +8,7 @@ import { NgProgressModule } from 'ngx-progressbar';
 import { BrowserModule } from '@angular/platform-browser';
 import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
 import { AlertModule } from 'ngx-alerts';
+import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
 
 @NgModule({
   declarations: [ColumnOneComponent,
@@ -19,7 +20,8 @@ import { AlertModule } from 'ngx-alerts';
     BrowserModule,
  
     // Specify your library as an import (set timeout to -1 for unlimited timeout, the message can only be closed by the user clicking on it)
-    AlertModule.forRoot({maxMessages: 5, timeout: 5000, position: 'right'})
+    AlertModule.forRoot({maxMessages: 5, timeout: 5000, position: 'right'}),
+    BsDropdownModule.forRoot()
   ],
   exports: [ColumnOneComponent],
 })
